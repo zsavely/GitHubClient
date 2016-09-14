b9a2bdd0535795a5d915b92d3e701f45e70d4b24
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-09-14 12:08:18 +0300

    Removed unused script files. [ci skip]

--- scripts/move-coverage-report.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/outputs/code-coverage/connected/coverage.ec /home/travis/build/zsavely/RxClipboard/rxclipboard/build/coverage_first.ec
-mv /home/travis/build/zsavely/RxClipboard/rxclipboard/build/reports/coverage/debug/report.xml /home/travis/build/zsavely/RxClipboard/rxclipboard/build/report_first.xml
