2ef7299a9e703ef416a0870e79a5a11a5ee90a4a
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-10-13 16:46:16 +0300

    Now setting tags correctly.

--- scripts/set_tags.sh
+++ scripts/set_tags.sh
@@ -5,11 +5,13 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   	if [ -z "$TRAVIS_TAG" ]; then
 	    echo -e "Starting to tag commit.\n"
 
+	    CURRENT_COMMIT_MESSAGE="$(git log --format=%B -n 1 HEAD)"
+
 	    git config --global user.email "travis@travis-ci.org"
 	    git config --global user.name "Travis"
 
 	    # Add tag and push to master.
-	    git tag -a v${TRAVIS_BUILD_NUMBER} -m "v${TRAVIS_BUILD_NUMBER}"
+	    git tag -a v${TRAVIS_BUILD_NUMBER} -m "v${TRAVIS_BUILD_NUMBER}" -m "${CURRENT_COMMIT_MESSAGE}"
 	    git push origin --tags
 	    git fetch origin
 
