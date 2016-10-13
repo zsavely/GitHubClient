09aae1ee1230395df9e51ad0efa1954426e7ea98
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-10-13 13:12:03 +0300

    Fixed issue when Application class wasn't used.

--- github-client/src/main/java/com/szagurskii/githubclient/app/GithubClientApp.kt
+++ github-client/src/main/java/com/szagurskii/githubclient/app/GithubClientApp.kt
@@ -4,10 +4,11 @@ import android.app.Application
 import com.szagurskii.githubclient.BuildConfig
 import timber.log.Timber
 
-class GithubClientApp : Application() {
+class GitHubClientApp : Application() {
 
   override fun onCreate() {
     super.onCreate()
+
     if (BuildConfig.DEBUG) {
       Timber.plant(Timber.DebugTree())
     }
